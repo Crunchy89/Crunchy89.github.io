c0213364e1c7393b8bdad2a27809a95470485e9c
crunchy89 <rocker.hunt@gmail.com>
2020-05-31 02:13:04 +0800

    Updates

---
+++ precache-manifest.49bffc984a97c06090ac523be68d4f65.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3189fca41ddcbb5a6ca67d6ca8e215a8",
+    "url": "/Crunchy89.github.io/index.html"
+  },
+  {
+    "revision": "936411d44a48b73130d1",
+    "url": "/Crunchy89.github.io/static/css/main.523f09c5.chunk.css"
+  },
+  {
+    "revision": "76129ce1319e6801814e",
+    "url": "/Crunchy89.github.io/static/js/2.9c8e02f5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Crunchy89.github.io/static/js/2.9c8e02f5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "936411d44a48b73130d1",
+    "url": "/Crunchy89.github.io/static/js/main.ce76d487.chunk.js"
+  },
+  {
+    "revision": "8c1f5b0910515608144e",
+    "url": "/Crunchy89.github.io/static/js/runtime-main.c1d0639f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Crunchy89.github.io/static/media/logo.5d5d9eef.svg"
+  }
+]);
